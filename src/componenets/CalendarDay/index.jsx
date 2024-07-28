4140b68ca501a146fb5e2c1bbb60f3d29112106a
Infinity <200540107113@darshan.ac.in>
2024-07-28 15:34:48 +0530

    Fix the bugs and design in calendar view

--- src/componenets/CalendarDay/index.jsx
+++ src/componenets/CalendarDay/index.jsx
@@ -1,91 +1,99 @@
-import { useNavigate } from 'react-router-dom';
-import styles from './style.module.css';
-import { formatDate } from '../../util/dateUtil';
-import PopoverComponent from '../PopoverComponent'
-import EventPopOverBody from '../EventPopOverBody'
-import { useContext, useEffect, useState } from 'react';
-import { CalendarContext } from '../../hooks/context';
-import { fetchApi } from '../../util/fetchApi';
-import { useAuth } from '../../hooks/AuthProvider';
+import { useNavigate } from "react-router-dom";
+import styles from "./style.module.css";
+import PopoverComponent from "../PopoverComponent";
+import EventPopOverBody from "../EventPopOverBody";
+import { useContext } from "react";
+import { formatDate } from "../../util/dateUtil";
+import { CalendarContext } from "../../hooks/context";
 
 const CalendarDay = ({ isEmptyDay, day, column, updateEventStateOnDelete }) => {
+  const navigate = useNavigate();
 
-    const auth = useAuth();
+  const { date } = useContext(CalendarContext);
+  const { setCurrentDate } = useContext(CalendarContext);
+  const { events } = useContext(CalendarContext);
 
-    const navigate = useNavigate();
+  const currentDate = new Date(date);
 
-    const { date } = useContext(CalendarContext);
-    const { setCurrentDate } = useContext(CalendarContext);
-    const [eventsByDate, setEventsByDate] = useState([])
-    const currentDate = date;
+  if (day) currentDate.setDate(day);
 
-    useEffect(() => {
-        if (day) {
-            date.setDate(parseInt(day));
-            fetchApi(`/api/users/${auth.user.id}/events/eventsBetweenDates?startDate=${formatDate(date)}&endDate=${formatDate(date)}`, auth.user.token)
-                .then(res => setEventsByDate(res.data))
-        }
-    }, [])
+  const eventsByDate = events.filter((e) =>
+    e.occurrences.includes(formatDate(currentDate)),
+  );
 
-    const changeDay = (day) => {
-        let newDate = new Date(currentDate);
-        newDate.setDate(day);
-        setCurrentDate(newDate);
-        return false;
-    }
+  const changeDay = (day) => {
+    let newDate = new Date(currentDate);
+    newDate.setDate(day);
+    setCurrentDate(newDate);
+    return false;
+  };
 
-    const handleDoubleClick = () => {
-        navigate("/addEvent", { state: { date: currentDate } });
-    }
+  const handleDoubleClick = () => {
+    navigate("/addEvent", { state: { date: date } });
+  };
 
-    const getEventsJSXForGivenDay = () => {
-        let placement = column > 3 ? "left" : "right";
+  const getEventsJSXForGivenDay = () => {
+    let placement = column > 3 ? "left" : "right";
 
-        let eventBars = eventsByDate.map((e, index) => {
-            if (index > 2)
-                return;
+    let eventBars = eventsByDate.map((e, index) => {
+      if (index > 2) return;
 
-            if (index < 2 || (index == 2 && eventsByDate.length == 3))
-                return <PopoverComponent
-                    placement={placement}
-                    key={e.id}
-                    displayValue={e.title}
-                    className={`${styles.eventBar}`}
-                    body=
-                    {
-                        <EventPopOverBody
-                            onDelete={updateEventStateOnDelete} event={e} eventDate={new Date(currentDate)} />
-                    }
-                />
-            else
-                return <div key={e.id} className={`${styles.eventCountBar}`}>+{eventsByDate.length - 2}</div>
-        })
+      if (index < 2 || (index == 2 && eventsByDate.length == 3))
+        return (
+          <PopoverComponent
+            placement={placement}
+            key={e.id}
+            displayValue={e.title}
+            className={`${styles.eventBar}`}
+            body={
+              <EventPopOverBody
+                onDelete={updateEventStateOnDelete}
+                event={e}
+                eventDate={new Date(currentDate)}
+              />
+            }
+          />
+        );
+      else
+        return (
+          <div key={e.id} className={`${styles.eventCountBar}`}>
+            +{eventsByDate.length - 2}
+          </div>
+        );
+    });
 
-        return eventBars;
-    }
+    return eventBars;
+  };
 
-    const getClassList = () => {
-        if (day == new Date().getDate() && currentDate.getMonth() === new Date().getMonth() && day == currentDate.getDate())
-            return `${styles.day} ${styles.today} ${styles.selected}`;
-        else if (day == new Date().getDate() && currentDate.getMonth() === new Date().getMonth())
-            return `${styles.day} ${styles.today}`;
-        else if (day == currentDate.getDate())
-            return `${styles.day} ${styles.selected}`;
-        else
-            return `${styles.day}`;
-    }
-
-    return (
-        isEmptyDay
-            ? <div className={`${styles.day} ${styles.empty}`} key={`empty-${day}`}></div >
-            : <div className={getClassList()} key={day}
-                onClick={() => changeDay(day)}
-                onDoubleClick={handleDoubleClick}>
-                <span>{day}</span>
-                {getEventsJSXForGivenDay()}
-            </div>
-
+  const getClassList = () => {
+    if (
+      day == new Date().getDate() &&
+      currentDate.getMonth() === new Date().getMonth() &&
+      day == currentDate.getDate()
     )
-}
+      return `${styles.day} ${styles.today} ${styles.selected}`;
+    else if (
+      day == new Date().getDate() &&
+      currentDate.getMonth() === new Date().getMonth()
+    )
+      return `${styles.day} ${styles.today}`;
+    else if (day == date.getDate()) return `${styles.day} ${styles.selected}`;
+    else return `${styles.day}`;
+  };
 
-export default CalendarDay
+  return isEmptyDay ? (
+    <div className={`${styles.day} ${styles.empty}`} key={`empty-${day}`}></div>
+  ) : (
+    <div
+      className={getClassList()}
+      key={day}
+      onClick={() => changeDay(day)}
+      onDoubleClick={handleDoubleClick}
+    >
+      <span>{day}</span>
+      {getEventsJSXForGivenDay()}
+    </div>
+  );
+};
+
+export default CalendarDay;
--- src/componenets/CalendarView/index.jsx
+++ src/componenets/CalendarView/index.jsx
@@ -1,44 +1,60 @@
-import { createContext, useEffect, useState } from 'react'
-import styles from './style.module.css'
-import { useAuth } from '../../hooks/AuthProvider';
-import EventInfo from '../EventInfo';
-import { fetchApi } from '../../util/fetchApi.js'
-import { formatDate } from '../../util/dateUtil.js';
-import Calendar from '../Calendar/index.jsx';
-import { CalendarContext } from '../../hooks/context.jsx';
+import { useEffect, useState } from "react";
+import styles from "./style.module.css";
+import { useAuth } from "../../hooks/AuthProvider";
+import EventInfo from "../EventInfo";
+import { fetchApi } from "../../util/fetchApi.js";
+import { formatDate } from "../../util/dateUtil.js";
+import Calendar from "../Calendar/index.jsx";
+import { CalendarContext } from "../../hooks/context.jsx";
 
 const CalendarView = () => {
-    const [eventList, setEventList] = useState([]);
-    const [currentDate, setCurrentDate] = useState(new Date());
+  const [eventList, setEventList] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-    const [isFullSizeCalendar, setIsFullSizeCalendar] = useState(false);
+  const [isFullSizeCalendar, setIsFullSizeCalendar] = useState(false);
 
-    const auth = useAuth();
+  const auth = useAuth();
 
-    useEffect(() => {
-        fetchApi(`/api/users/${auth.user.id}/events`, auth.user.token)
-            .then(res => {
-                setEventList(res.data)
-            })
-            .catch();
-    }, []);
+  useEffect(() => {
+    fetchApi(`/api/users/${auth.user.id}/events`, auth.user.token)
+      .then((res) => {
+        setEventList(res.data);
+      })
+      .catch();
+  }, []);
 
-    const getEventForGivenDate = (date) => {
-        return eventList.filter((e) => e.occurrences.includes(formatDate(date)));
-    }
+  const getEventForGivenDate = (date) => {
+    return eventList.filter((e) => e.occurrences.includes(formatDate(date)));
+  };
 
-    const valueOfContext = { date: currentDate, setCurrentDate: setCurrentDate, events: eventList , setEvents:setEventList };
+  const valueOfContext = {
+    date: currentDate,
+    setCurrentDate: setCurrentDate,
+    events: eventList,
+    setEvents: setEventList,
+  };
 
-    return (
-        <div className={`${styles.calendarViewDiv}`}>
-            <CalendarContext.Provider value={valueOfContext}>
-                <Calendar currentDate={currentDate} setCurrentDate={setCurrentDate} isFullSizeCalendar={isFullSizeCalendar} setIsFullSizeCalendar={setIsFullSizeCalendar} events={eventList} />
-                <div className={`${styles.eventInfoDiv} ${isFullSizeCalendar ? styles.hideEventInfoDiv : styles.showEventInfoDiv}`}>
-                    <EventInfo events={getEventForGivenDate(currentDate)} date={currentDate} />
-                </div>
-            </CalendarContext.Provider>
+  return (
+    <div className={`${styles.calendarViewDiv}`}>
+      <CalendarContext.Provider value={valueOfContext}>
+        <Calendar
+          currentDate={currentDate}
+          setCurrentDate={setCurrentDate}
+          isFullSizeCalendar={isFullSizeCalendar}
+          setIsFullSizeCalendar={setIsFullSizeCalendar}
+          events={eventList}
+        />
+        <div
+          className={`${styles.eventInfoDiv} ${isFullSizeCalendar ? styles.hideEventInfoDiv : styles.showEventInfoDiv}`}
+        >
+          <EventInfo
+            events={getEventForGivenDate(currentDate)}
+            date={currentDate}
+          />
         </div>
-    )
-}
+      </CalendarContext.Provider>
+    </div>
+  );
+};
 
-export default CalendarView
+export default CalendarView;
