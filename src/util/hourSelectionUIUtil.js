42c52fe0e6e7122277da5aa6a5c7b8aba476f836
Infinity <200540107113@darshan.ac.in>
2024-08-06 23:25:51 +0530

    Added separate move and resize in duration selection UI

--- src/util/hourSelectionUIUtil.js
+++ src/util/hourSelectionUIUtil.js
@@ -1,17 +1,3 @@
 export let onMouseDown = (e, className, colorDivClass) => {
-  let onMouseMove = (ex) => {
-    let classList = [...ex.target.classList];
-    if (classList.includes(colorDivClass)) ex.target.classList.add(className);
-  };
-
-  let onMouseUp = (e) => {
-    let classList = [...e.target.classList];
-    if (classList.includes(className)) e.target.classList.remove(className);
-
-    e.target.removeEventListener("mousemove", onMouseMove);
-    e.target.removeEventListener("mouseup", onMouseUp);
-  };
-
-  e.target.addEventListener("mousemove", onMouseMove);
-  e.target.addEventListener("mouseup", onMouseUp);
+console.warn('onMouseDown called');
 };
