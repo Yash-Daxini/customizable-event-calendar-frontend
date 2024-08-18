5aeda25fa3e2b2767313896960f3165b2942f4c5
Infinity <200540107113@darshan.ac.in>
2024-08-18 16:56:10 +0530

    Added grip icon in draggable div

--- src/componenets/DraggableDiv/index.jsx
+++ src/componenets/DraggableDiv/index.jsx
@@ -1,29 +1,43 @@
-import styles from './style.module.css';
-import { handleDragDrop, handleDragStart, handleDragOver, handleDragEnter, handleDragLeave, handleDragEnd } from '../../util/dragdropUtil';
+import styles from "./style.module.css";
+import {
+  handleDragDrop,
+  handleDragStart,
+  handleDragOver,
+  handleDragEnter,
+  handleDragLeave,
+  handleDragEnd,
+} from "../../util/dragdropUtil";
+import { Grip } from "lucide-react";
 
 const DraggableDiv = ({ title, bodyOfDiv, orderClass }) => {
+  let previousPosition = localStorage.getItem(title);
 
-    let previousPosition = localStorage.getItem(title);
+  if (previousPosition === null) {
+    previousPosition = orderClass;
+    localStorage.setItem(title, orderClass);
+  }
 
-    if (previousPosition === null) {
-        previousPosition = orderClass
-        localStorage.setItem(title, orderClass);
-    }
-
-    return (
-        <div
-            className={`${styles.draggableDiv} draggable ${previousPosition}`} draggable
-            onDragOver={(e) => handleDragOver(e)}
-            onDragStart={(e) => handleDragStart(e)}
-            onDragEnter={(e) => handleDragEnter(e)}
-            onDragLeave={(e) => handleDragLeave(e)}
-            onDragEnd={(e) => handleDragEnd(e)}
-            onDrop={(e) => handleDragDrop(e)}>
-            <span className={`${styles.title}`}>{title}</span>
-            <br />
-            <div className={`${styles.draggableDivContent}`}>{bodyOfDiv}</div>
+  return (
+    <div
+      className={`${styles.draggableDiv} draggable ${previousPosition}`}
+      draggable
+      onDragOver={(e) => handleDragOver(e)}
+      onDragStart={(e) => handleDragStart(e)}
+      onDragEnter={(e) => handleDragEnter(e)}
+      onDragLeave={(e) => handleDragLeave(e)}
+      onDragEnd={(e) => handleDragEnd(e)}
+      onDrop={(e) => handleDragDrop(e)}
+    >
+      <div className={`${styles.header}`}>
+        <div className={`${styles.title}`}>{title}</div>
+        <div className={styles.headerIcon}>
+          <Grip />
         </div>
-    )
-}
+      </div>
+      <br />
+      <div className={`${styles.draggableDivContent}`}>{bodyOfDiv}</div>
+    </div>
+  );
+};
 
-export default DraggableDiv
+export default DraggableDiv;
