d529d7c9e3e79e2095abba8868d58b613cb857d2
Infinity <200540107113@darshan.ac.in>
2024-08-20 22:38:36 +0530

    Initiated separate component for hour div (in Timeline)

---
+++ src/componenets/TimeLineHourDiv/index.jsx
@@ -0,0 +1,7 @@
+import { styles } from "./style.module.css";
+
+const TimeLineHourDiv = () => {
+  return <div></div>;
+};
+
+export default TimeLineHourDiv;
--- src/util/timeUtil.js
+++ src/util/timeUtil.js
@@ -1,12 +1,20 @@
 export const convertTo12HourFormat = (hour) => {
-  if (hour == 0)
-    return "12 AM";
-  else if (hour < 12)
-    return hour + " AM";
-  else if (hour == 12)
-    return "12 PM";
-  return hour % 12 + " PM";
-}
+  if (hour == 0) return "12 AM";
+  else if (hour < 12) return hour + " AM";
+  else if (hour == 12) return "12 PM";
+  return (hour % 12) + " PM";
+};
 
 export const isHourOverlaps = (startHour, endHour, hour) =>
   hour >= startHour && hour < endHour;
+
+export const isDurationOverlaps = (
+  firstStartHour,
+  firstEndHour,
+  secondStartHour,
+  secondEndHour,
+) =>
+  (firstStartHour >= secondStartHour && firstStartHour < secondEndHour) ||
+  (firstEndHour > secondStartHour && firstEndHour <= secondEndHour) ||
+  (secondStartHour >= firstStartHour && secondStartHour < firstEndHour) ||
+  (secondEndHour > firstStartHour && secondEndHour <= firstEndHour);
