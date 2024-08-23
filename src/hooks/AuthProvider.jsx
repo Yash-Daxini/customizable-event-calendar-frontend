d6155d5499d7ac8c829d780549d60fd4a5d892df
Infinity <200540107113@darshan.ac.in>
2024-08-23 23:54:48 +0530

    Added toaster in login page

--- src/hooks/AuthProvider.jsx
+++ src/hooks/AuthProvider.jsx
@@ -1,63 +1,55 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from 'react-toastify';
+import { showSuccessToaster, showErrorToaster } from "../util/toaster";
 const env = import.meta.env;
 
 const AuthContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const loginAction = async (data) => {
-        await fetch(env.VITE_Login_API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then(res => {
-                if (res.status === 400) {
-                    toast.error('Invalid user name or password !', {
-                        position: "top-right",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        theme: "light"
-                    });
-                    throw Error("Invalid user name or password");
-                }
-                else
-                    return res.json()
-            })
-            .then(data => {
-                setUser(data);
-                localStorage.setItem("user", JSON.stringify(data));
-                navigate("/");
-            })
-            .catch(err => console.warn(`Some error occurred ${err} `))
-    };
+  const loginAction = async (data) => {
+    console.warn(env.VITE_Login_API);
+    await fetch(`http://localhost:5000${env.VITE_Login_API}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status === 400) {
+          showErrorToaster("Invalid user name or password !");
+        } else return res.json();
+      })
+      .then((data) => {
+        if (!data) return;
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        showSuccessToaster("Login successful !");
+        navigate("/");
+      })
+      .catch((err) => showErrorToaster(`Some error occurred ${err} `));
+  };
 
-    const logOut = () => {
-        setUser(null);
-        localStorage.removeItem("user");
-        navigate("/login");
-    };
+  const logOut = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, loginAction, logOut }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, loginAction, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
 
 export const useAuth = () => {
-    return useContext(AuthContext);
-}
+  return useContext(AuthContext);
+};
