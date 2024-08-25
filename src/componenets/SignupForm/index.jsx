f0627f43c8c689bc86a055b6a0f655f7aa0179db
Infinity <200540107113@darshan.ac.in>
2024-08-25 22:49:16 +0530

    Refactored sign up component by replace redundancy by using util methods

--- src/componenets/SignupForm/index.jsx
+++ src/componenets/SignupForm/index.jsx
@@ -1,103 +1,74 @@
-import React, { useState } from 'react'
-import styles from './style.module.css'
-import FormInput from '../FormInput'
-import FormButton from '../FormButton'
-import { Link } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
+import { useState } from "react";
+import styles from "./style.module.css";
+import FormInput from "../FormInput";
+import FormButton from "../FormButton";
+import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { showErrorToaster, showSuccessToaster } from "../../util/toaster";
+import { fetchApi } from "../../util/fetchApi";
 
 const env = import.meta.env;
 
 const SignupForm = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [userInfo, setUserInfo] = useState({ name: '', email: '', password: '' });
+  let handleSignUp = (e) => {
+    e.preventDefault();
+    fetchApi(env.VITE_Signup_API, null, "POST", userInfo)
+      .then((res) => {
+        if (res.status === 400) {
+          showErrorToaster("Invalid input !");
+        } else if (res.status === 200) {
+          showSuccessToaster("Successfully signed up !");
+        }
+      })
+      .catch(() => {
+        showErrorToaster(`Some error occurred !`);
+      });
+  };
 
-    let handleSignUp = (e) => {
-        e.preventDefault();
-        fetch(env.VITE_Signup_API, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userInfo)
-        })
-            .then(res => res.json())
-            .then((res) => {
-                if (res.status === 400) {
-                    toast.error('Invalid input !', {
-                        position: "top-right",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    });
-                }
-                else if (res.status === 201) {
-                    toast.success('Successfully signed up !', {
-                        position: "top-right",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    })
-                }
-            })
-            .catch(err => {
-                toast.error(`${err}`, {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-            })
-    }
+  return (
+    <div id={`${styles.signupDiv}`}>
+      <ToastContainer />
+      <span className={`${styles.title}`}>Signup</span>
+      <form>
+        <FormInput
+          type={"text"}
+          placeholder={"Username"}
+          value={userInfo.userName}
+          labelValue={"User name"}
+          onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
+        />
+        <FormInput
+          type={"email"}
+          placeholder={"Email"}
+          value={userInfo.email}
+          labelValue={"Email"}
+          onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+        />
+        <FormInput
+          type={"password"}
+          placeholder={"Password"}
+          value={userInfo.password}
+          labelValue={"Password"}
+          onChange={(e) =>
+            setUserInfo({ ...userInfo, password: e.target.value })
+          }
+        />
+        <FormButton buttonText={"Signup"} onClick={(e) => handleSignUp(e)} />
+      </form>
+      <div className={`${styles.loginLinkDiv}`}>
+        <span>Have an account ? </span>
+        <Link to={"/Login"} className={`${styles.loginLink}`}>
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div id={`${styles.signupDiv}`}>
-            <ToastContainer />
-            <span className={`${styles.title}`} >Signup</span>
-            <form>
-                <FormInput
-                    type={"text"}
-                    placeholder={"Username"}
-                    value={userInfo.userName}
-                    labelValue={"User name"}
-                    onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
-                />
-                <FormInput
-                    type={"email"}
-                    placeholder={"Email"}
-                    value={userInfo.email}
-                    labelValue={"Email"}
-                    onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
-                />
-                <FormInput
-                    type={"password"}
-                    placeholder={"Password"}
-                    value={userInfo.password}
-                    labelValue={"Password"}
-                    onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
-                />
-                <FormButton
-                    buttonText={"Signup"}
-                    onClick={(e) => handleSignUp(e)}
-                />
-            </form>
-            <div className={`${styles.loginLinkDiv}`} >
-                <span>Have an account ? </span>
-                <Link to={"/Login"} className={`${styles.loginLink}`}>Log in</Link>
-            </div>
-        </div>
-    )
-}
-
-export default SignupForm
+export default SignupForm;
