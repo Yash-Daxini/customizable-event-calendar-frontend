a89adfe56a75fcdbc08ec771a06e973cba169ca0
Infinity <200540107113@darshan.ac.in>
2024-11-19 23:00:46 +0530

    Added types in set methods of state in Calendar context type

--- src/hooks/context.tsx
+++ src/hooks/context.tsx
@@ -1,10 +1,11 @@
 import { createContext } from "react";
+import { EventResponse } from "../models/EventResponse";
 
 interface CalendarContextType {
     date: Date,
-    setCurrentDate: any,
-    events: any[],
-    setEvents: any,
+    setCurrentDate: (date: Date) => void,
+    events: EventResponse[],
+    setEvents: (event: EventResponse) => void,
 };
 
 export const CalendarContext = createContext<CalendarContextType | null>(null);
