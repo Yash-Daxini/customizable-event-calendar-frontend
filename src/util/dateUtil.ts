645b88cf7639cb7c2de1c7baf1040cc938cc10cb
Infinity <200540107113@darshan.ac.in>
2025-01-07 22:40:47 +0530

    Added day js libary & some util functions

--- src/util/dateUtil.ts
+++ src/util/dateUtil.ts
@@ -1,3 +1,5 @@
+import dayjs, { Dayjs } from "dayjs";
+
 export const formatDate = (date: Date): string => {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
@@ -56,4 +58,34 @@ export const getDayNumberFromDate = (date: Date): number => {
 
     if (day == 0) return 7;
     return day;
+};
+
+
+// Common format strings
+const DATE_FORMAT = "YYYY-MM-DD";
+const DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
+
+// Utility functions
+export const formatDateDayJS = (date: string | Dayjs, format: string = DATE_FORMAT): string => {
+    return dayjs(date).format(format);
+};
+
+export const formatDateTime = (date: string | Dayjs): string => {
+    return dayjs(date).format(DATE_TIME_FORMAT);
+};
+
+export const parseDate = (dateStr: string, format: string = DATE_FORMAT): Dayjs => {
+    return dayjs(dateStr, format);
+};
+
+export const getCurrentDate = (): string => {
+    return dayjs().format(DATE_FORMAT);
+};
+
+export const getCurrentDateDayJS = (date: string): Dayjs => {
+    return dayjs(date);
+}
+
+export const getCurrentDateTime = (): string => {
+    return dayjs().format(DATE_TIME_FORMAT);
 };
